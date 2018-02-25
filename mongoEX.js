fe90866a41ae8c3f7b8a6ff9516a7b74f1befabb
Naeim Zarei <naeimz@live.unc.edu>
2018-02-24 20:38:08 -0500

    Test MongoDB

--- mongoEX.js
+++ mongoEX.js
@@ -1,9 +1,18 @@
-var MongoClient = require('mongodb').MongoClient;
-
-var url = 'mongodb://localhost:27017/learnMongo'
-
-MongoClient.connect(url, function(err, db) {
-	console.log("Connected successfully to server")
-
-	db.close();
-})
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+ 
+// Connection URL
+const url = 'mongodb://192.168.142.3:27017';
+ 
+// Database Name
+const dbName = 'sampledb';
+ 
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+ 
+  const db = client.db(dbName);
+ 
+  client.close();
+});
